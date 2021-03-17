12ffb0bf0ef223944df6cce1fb31ed68270bc2e9
Teemu Pääkkönen <oamaok@gmail.com>
2021-03-17 20:41:19 +0200

    Add initial test setup

---
+++ test/createApi.spec.ts
@@ -0,0 +1,77 @@
+import { spawn } from 'child_process'
+
+const HEADER = `
+import {
+  createApi,
+  CreateApiResponseType,
+  CreateApiInterfaceType,
+  CreateApiRequestType,
+} from './server/createApi'
+`
+
+const FOOTER = `
+type ApiInterface = CreateApiInterfaceType<typeof api>
+type ApiResponse = CreateApiResponseType<typeof api>
+type ApiRequest = CreateApiRequestType<typeof api>
+`
+
+const verbose = typeof process.env.VERBOSE_TESTS !== 'undefined'
+
+const doesCompile = (code: string) =>
+  new Promise((resolve) => {
+    const tsc = spawn('npx', ['ts-node'])
+    tsc.stdin.write(HEADER + code + FOOTER)
+    tsc.stdin.end()
+    if (verbose) {
+      tsc.stderr.on('data', (data) => console.error(data.toString()))
+      tsc.stdout.on('data', (data) => console.log(data.toString()))
+    }
+    tsc.on('exit', (code) => resolve(code === 0))
+  })
+
+const shouldNotCompile = async (code: string) => {
+  if (await doesCompile(code)) {
+    throw new Error('Code did compile when it should not')
+  }
+}
+
+const shouldCompile = async (code: string) => {
+  if (!(await doesCompile(code))) {
+    throw new Error('Code did not compile when it should')
+  }
+}
+
+describe('createApi', () => {
+  it('should not compile with non-json-safe parameters', async () => {
+    await shouldNotCompile(`
+      const api = createApi({
+        test(arg: Uint8Array): number { return 0 }
+      })
+    `)
+  })
+
+  it('should not compile with non-json-safe return value', async () => {
+    await shouldNotCompile(`
+      const api = createApi({
+        test(arg: number): Uint8Array { return new Uint8Array() }
+      })
+    `)
+  })
+
+  it('should compile with array types', async () => {
+    await shouldCompile(`
+        const api = createApi({
+          test(a: string[], b: number[], c: boolean[]): number { return 0 }
+        })
+      `)
+  })
+
+  // TODO: Add support for no params
+  it.skip('should compile with no parameters', async () => {
+    await shouldCompile(`
+      const api = createApi({
+        test(): number { return 0 }
+      })
+    `)
+  })
+})
